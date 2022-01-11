75aaf0c0e998aa0434c1971a905679bac2e18fcc
Guofeng Zhang <guofengzh@gmail.com>
2022-01-11 15:58:38 +0800

    Create Implies.scala

---
+++ src/main/scala/logic/Implies.scala
@@ -0,0 +1,19 @@
+package logic
+
+/**
+ * "a implies b" translating into "!a || b"
+ */
+object Implies {  
+  extension (a: Boolean) {
+     def implies(b: => Boolean) : Boolean  = { 
+        !a || b 
+     }
+  }
+
+  def main(args:Array[String]) : Unit = {
+      val v1 = true implies { println("evaluated"); true } 
+      val v2 = false implies { println("evaluated"); true } 
+      println(v1)
+      println(v2)
+  }
+}
