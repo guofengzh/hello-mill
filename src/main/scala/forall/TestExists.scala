873bea7361c8dd90f8508296f4bcff4f611f800c
Guofeng Zhang <guofengzh@gmail.com>
2022-01-13 16:16:31 +0800

    exists & forall

---
+++ src/main/scala/forall/TestExists.scala
@@ -0,0 +1,24 @@
+package forall
+
+object ExistsMain {
+
+  /*  
+  def foo(unapplied: List[Exists[[A] => A => (A, A => String)]]): List[String] =
+    unapplied map { u =>
+      val (v, f) = u.value
+      f(v)
+    }  
+
+
+  def main(args: Array[String]):Unit = {
+      val r = foo(
+            List(
+              Exists[[A] => A => (A, A => String)]((42, (i: Int) => i.toString)),
+              Exists[[A] => A => (A, A => String)](("hi", (s: String) => s))
+            )
+        )
+      println(r)
+  } 
+  */
+}
+
--- src/main/scala/forall/TestForAll.scala
+++ src/main/scala/forall/TestForAll.scala
@@ -1,19 +1,19 @@
 package forall
 
-type PF = [A] => A => List[A]
-type PP[A] = PF
+object ForAllMain {
+    
+  def foo[A](a: A): List[A] = List(a)
 
-def foo[A](a: A): List[A] = List(a)
-
-/*
-def bippy(
+  /*
+  def bippy(
     s: String,
     i: Int,
-    f: Forall[PP]): (List[String], List[Int]) =
+    f: Forall[[A] => A => List[A]]): (List[String], List[Int]) =
         (f[String](s), f[Int](i))    
 
-@main def TestForAll: Unit = {
-    val r = bippy("hi", 42, Forall[PP]([A] => (a:A)  => foo(a)))   // => (List("hi"), List(42))
+  def main(args:Array[String]): Unit = {
+    val r = bippy("hi", 42, Forall[[A] => A => List[A]]([A] => (a:A)  => foo(a)))   // => (List("hi"), List(42))
     println(r)
-}
+  }
 */
+}
