3c28998d0190ee12da806f5f9bab3650f60630c5
Guofeng Zhang <guofengzh@gmail.com>
2022-01-17 16:05:26 +0800

    using type-lambda in Scala 3

--- src/main/scala/forall/TestExists.scala
+++ src/main/scala/forall/TestExists.scala
@@ -1,24 +1,23 @@
 package forall
 
 object ExistsMain {
-
-  /*  
-  def foo(unapplied: List[Exists[[A] => A => (A, A => String)]]): List[String] =
+  
+  def foo(unapplied: List[∃[[A] =>> (A, A => String)]]): List[String] =
     unapplied map { u =>
       val (v, f) = u.value
       f(v)
-    }  
+    }
 
 
   def main(args: Array[String]):Unit = {
-      val r = foo(
-            List(
-              Exists[[A] => A => (A, A => String)]((42, (i: Int) => i.toString)),
-              Exists[[A] => A => (A, A => String)](("hi", (s: String) => s))
-            )
-        )
-      println(r)
+
+    val r = foo(
+              List(
+                ∃[[A] =>> (A, A => String)]((42, (i: Int) => i.toString)),
+                ∃[[A] =>> (A, A => String)](("hi", (s: String) => s))))
+
+      println(r)  // List(42, hi)
   } 
-  */
+  
 }
 
