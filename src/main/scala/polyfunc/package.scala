2315cadd8189f13a2e0bef5a14e738ae407d7930
Guofeng Zhang <guofengzh@gmail.com>
2022-01-29 11:20:19 +0800

    add comment

--- src/main/scala/polyfunc/package.scala
+++ src/main/scala/polyfunc/package.scala
@@ -1,13 +1,27 @@
 package polyfunc
 
+/* polymorphic function type: one type parameter [A] */
 def foo[A](a: A): List[A] = List(a)
 
-val f:[A] => A => List[A] 
-         = [A] => (a:A)  => foo(a)
+val f:[A] => A      => List[A] = 
+      [A] => (a:A)  => foo(a)
+
+/* polymorphic function type: multiple type parameters [A, B] */
+def goo[A, B](a: A) : Either[A, B] = Left(a)
+
+val g:[A, B] => A      => Either[A, B] = 
+      [A, B] => (a: A) => goo(a)
 
 @main def TestPf: Unit = {
     println(f(1))
     println(f("hi"))
 }
 
+/* type lambda: one type parameter */
 type F = [X] =>> (X, X)
+
+/* type lambda: multiple paramters */
+type G = [X, Y] =>> Map[Y, X]
+
+/* type lambda: high ordered */ 
+type E = [X] =>> [Y] =>> Either[X, Y]
